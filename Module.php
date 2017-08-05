3a2751f94d960b5859eebd4837447641a34732b5
hrupin <hrupin@i.ua>
2017-08-05 22:13:36 +0300

    Update Module.php

--- Module.php
+++ Module.php
@@ -6,7 +6,7 @@ use yii\base\Module as BaseModule;
 class Module extends BaseModule
 {
     const VERSION = '0.0.1';
-    const STRATEGY_COMMENTS
+    const STRATEGY_COMMENTS;
     const STRATEGY_COMMENTS_DEFAULT = 1;    
     public $admins = [];
     public $adminPermission;
