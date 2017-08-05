9636329892eb5ee57be6f25d4fd98b51710aed1e
hrupin <hrupin@i.ua>
2017-08-05 15:40:14 +0300

    Update Mailer.php

--- Mailer.php
+++ Mailer.php
@@ -10,200 +10,38 @@ class Mailer extends Component
     public $viewPath = '@hrupin/blog/views/mail';
     public $sender;
     public $mailerComponent;
-    protected $welcomeSubject;
-    protected $newPasswordSubject;
-    protected $confirmationSubject;
-    protected $reconfirmationSubject;
-    protected $recoverySubject;
+    protected $commentSubject;
     protected $module;
     
-    public function getWelcomeSubject()
+    public function getCommentSubject()
     {
-        if ($this->welcomeSubject == null) {
-            $this->setWelcomeSubject(Yii::t('user', 'Welcome to {0}', Yii::$app->name));
+        if ($this->commentSubject == null) {
+            $this->setCommentSubject(Yii::t('blog', 'New comment to {0}', Yii::$app->name));
         }
-        return $this->welcomeSubject;
+        return $this->commentSubject;
     }
-    /**
-     * @param string $welcomeSubject
-     */
-    public function setWelcomeSubject($welcomeSubject)
+
+    public function setCommentSubject($commentSubject)
     {
-        $this->welcomeSubject = $welcomeSubject;
+        $this->commentSubject = $commentSubject;
     }
-    /**
-     * @return string
-     */
-    public function getNewPasswordSubject()
-    {
-        if ($this->newPasswordSubject == null) {
-            $this->setNewPasswordSubject(Yii::t('user', 'Your password on {0} has been changed', Yii::$app->name));
-        }
-        return $this->newPasswordSubject;
-    }
-    /**
-     * @param string $newPasswordSubject
-     */
-    public function setNewPasswordSubject($newPasswordSubject)
-    {
-        $this->newPasswordSubject = $newPasswordSubject;
-    }
-    /**
-     * @return string
-     */
-    public function getConfirmationSubject()
-    {
-        if ($this->confirmationSubject == null) {
-            $this->setConfirmationSubject(Yii::t('user', 'Confirm account on {0}', Yii::$app->name));
-        }
-        return $this->confirmationSubject;
-    }
-    /**
-     * @param string $confirmationSubject
-     */
-    public function setConfirmationSubject($confirmationSubject)
-    {
-        $this->confirmationSubject = $confirmationSubject;
-    }
-    /**
-     * @return string
-     */
-    public function getReconfirmationSubject()
-    {
-        if ($this->reconfirmationSubject == null) {
-            $this->setReconfirmationSubject(Yii::t('user', 'Confirm email change on {0}', Yii::$app->name));
-        }
-        return $this->reconfirmationSubject;
-    }
-    /**
-     * @param string $reconfirmationSubject
-     */
-    public function setReconfirmationSubject($reconfirmationSubject)
-    {
-        $this->reconfirmationSubject = $reconfirmationSubject;
-    }
-    /**
-     * @return string
-     */
-    public function getRecoverySubject()
-    {
-        if ($this->recoverySubject == null) {
-            $this->setRecoverySubject(Yii::t('user', 'Complete password reset on {0}', Yii::$app->name));
-        }
-        return $this->recoverySubject;
-    }
-    /**
-     * @param string $recoverySubject
-     */
-    public function setRecoverySubject($recoverySubject)
-    {
-        $this->recoverySubject = $recoverySubject;
-    }
-    /** @inheritdoc */
+    
     public function init()
     {
-        $this->module = Yii::$app->getModule('user');
+        $this->module = Yii::$app->getModule('blog');
         parent::init();
     }
-    /**
-     * Sends an email to a user after registration.
-     *
-     * @param User  $user
-     * @param Token $token
-     * @param bool  $showPassword
-     *
-     * @return bool
-     */
-    public function sendWelcomeMessage(User $user, Token $token = null, $showPassword = false)
+
+    public function sendCommentMessage(Comment $comment)
     {
         return $this->sendMessage(
-            $user->email,
-            $this->getWelcomeSubject(),
-            'welcome',
-            ['user' => $user, 'token' => $token, 'module' => $this->module, 'showPassword' => $showPassword]
+            $comment->email,
+            $this->getCommentSubject(),
+            'comment',
+            ['user' => $user, 'module' => $this->module]
         );
     }
-    /**
-     * Sends a new generated password to a user.
-     *
-     * @param User  $user
-     * @param Password $password
-     *
-     * @return bool
-     */
-    public function sendGeneratedPassword(User $user, $password)
-    {
-        return $this->sendMessage(
-            $user->email,
-            $this->getNewPasswordSubject(),
-            'new_password',
-            ['user' => $user, 'password' => $password, 'module' => $this->module]
-        );
-    }
-    /**
-     * Sends an email to a user with confirmation link.
-     *
-     * @param User  $user
-     * @param Token $token
-     *
-     * @return bool
-     */
-    public function sendConfirmationMessage(User $user, Token $token)
-    {
-        return $this->sendMessage(
-            $user->email,
-            $this->getConfirmationSubject(),
-            'confirmation',
-            ['user' => $user, 'token' => $token]
-        );
-    }
-    /**
-     * Sends an email to a user with reconfirmation link.
-     *
-     * @param User  $user
-     * @param Token $token
-     *
-     * @return bool
-     */
-    public function sendReconfirmationMessage(User $user, Token $token)
-    {
-        if ($token->type == Token::TYPE_CONFIRM_NEW_EMAIL) {
-            $email = $user->unconfirmed_email;
-        } else {
-            $email = $user->email;
-        }
-        return $this->sendMessage(
-            $email,
-            $this->getReconfirmationSubject(),
-            'reconfirmation',
-            ['user' => $user, 'token' => $token]
-        );
-    }
-    /**
-     * Sends an email to a user with recovery link.
-     *
-     * @param User  $user
-     * @param Token $token
-     *
-     * @return bool
-     */
-    public function sendRecoveryMessage(User $user, Token $token)
-    {
-        return $this->sendMessage(
-            $user->email,
-            $this->getRecoverySubject(),
-            'recovery',
-            ['user' => $user, 'token' => $token]
-        );
-    }
-    /**
-     * @param string $to
-     * @param string $subject
-     * @param string $view
-     * @param array  $params
-     *
-     * @return bool
-     */
+    
     protected function sendMessage($to, $subject, $view, $params = [])
     {
         $mailer = $this->mailerComponent === null ? Yii::$app->mailer : Yii::$app->get($this->mailerComponent);
