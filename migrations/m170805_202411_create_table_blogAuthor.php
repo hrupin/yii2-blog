719b95b2803bfaf9190cafbae3ba46d4ac28fa57
hrupin <hrupin@i.ua>
2017-08-05 23:47:27 +0300

    Update m170805_202411_create_table_blogAuthor.php

--- migrations/m170805_202411_create_table_blogAuthor.php
+++ migrations/m170805_202411_create_table_blogAuthor.php
@@ -13,16 +13,16 @@ class m170805_202411_create_table_blogAuthor extends Migration
         }
         $this->createTable('{{%blogAuthor}}', [
             'id' => Schema::TYPE_PK,
-            'id_author' => Schema::TYPE_INTEGER,
-            'id_user' => Schema::TYPE_INTEGER,
+            'id_author' => Schema::TYPE_INTEGER . ' NOT NULL',
+            'id_user' => Schema::TYPE_INTEGER . ' NULL',
             'lang' => Schema::TYPE_STRING . '(6) NOT NULL',
             'name' => Schema::TYPE_STRING . '(255) NOT NULL',
             'avatar' => Schema::TYPE_STRING . '(255) NOT NULL',
             'description' => Schema::TYPE_STRING . '(500) NULL',
             'about' => Schema::TYPE_TEXT . '(5000) NULL',
             'contact' => Schema::TYPE_TEXT . '(1000) NULL',
-            'dateCreated' => Schema::TYPE_INTEGER,
-            'dateUpdated' => Schema::TYPE_INTEGER,
+            'dateCreated' => Schema::TYPE_INTEGER . '(12) NOT NULL',
+            'dateUpdated' => Schema::TYPE_INTEGER . '(12) NOT NULL',
         ], $tableOptions);
     }
 
