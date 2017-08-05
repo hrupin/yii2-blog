598efbf4ab8ec30991e39e8c7a419ce80d0a7968
hrupin <hrupin.work@yandex.ru>
2017-08-06 01:17:49 +0300

    Add files via upload

---
+++ models/BlogSeoQuery.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace frontend\models;
+
+/**
+ * This is the ActiveQuery class for [[\app\models\BlogSeo]].
+ *
+ * @see \app\models\BlogSeo
+ */
+class BlogSeoQuery extends \yii\db\ActiveQuery
+{
+    /*public function active()
+    {
+        return $this->andWhere('[[status]]=1');
+    }*/
+
+    /**
+     * @inheritdoc
+     * @return \app\models\BlogSeo[]|array
+     */
+    public function all($db = null)
+    {
+        return parent::all($db);
+    }
+
+    /**
+     * @inheritdoc
+     * @return \app\models\BlogSeo|array|null
+     */
+    public function one($db = null)
+    {
+        return parent::one($db);
+    }
+}
