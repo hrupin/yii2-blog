6edd2b8f38e41c33564bc4332096ea1ad0325ea1
hrupin <hrupin@i.ua>
2017-08-05 23:18:54 +0300

    Create m170805_201536_create_table_blogSeo.php

---
+++ migrations/m170805_201536_create_table_blogSeo.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m170805_201536_create_table_blogSeo extends Migration
+{
+    public function safeUp()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
+        }
+        $this->createTable('{{%blogSeo}}', [
+            'id' => Schema::TYPE_PK,
+            'id_seo' => Schema::TYPE_INTEGER,
+            'lang' => Schema::TYPE_STRING . '(6) NOT NULL',
+            'tag' => Schema::TYPE_STRING . '(15) NOT NULL',
+            'value' => Schema::TYPE_TEXT . '(150) NULL',
+            'dateCreated' => Schema::TYPE_INTEGER,
+            'dateUpdated' => Schema::TYPE_INTEGER,
+        ], $tableOptions);
+    }
+
+    public function safeDown()
+    {
+        $this->dropTable('{{%blogSeo}}');
+    }
+}
