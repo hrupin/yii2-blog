6ef8e1091e37cdb75f3d259ed8b0a309420d508a
hrupin <hrupin@i.ua>
2017-08-05 23:37:30 +0300

    Create m170805_202958_create_table_blogPost.php

---
+++ migrations/m170805_202958_create_table_blogPost.php
@@ -0,0 +1,37 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m170805_202958_create_table_blogPost extends Migration
+{
+    public function safeUp()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
+        }
+        $this->createTable('{{%blogPost}}', [
+            'id' => Schema::TYPE_PK,
+            'id_post' => Schema::TYPE_INTEGER,
+            'id_category' => Schema::TYPE_INTEGER,
+            'id_author' => Schema::TYPE_INTEGER,
+            'id_tag' => Schema::TYPE_STRING . '(100)',
+            'id_seo' => Schema::TYPE_INTEGER . '(100)',
+            'lang' => Schema::TYPE_STRING . '(6) NOT NULL',
+            'title' => Schema::TYPE_STRING . '(255) NOT NULL',
+            'description' => Schema::TYPE_STRING . '(500) NULL',
+            'content' => Schema::TYPE_TEXT . '(15000) NULL',
+            'thumbnail' => Schema::TYPE_STRING . '(255) NULL',
+            'img' => Schema::TYPE_STRING . '(255) NULL',
+            'permissionToComment' => Schema::TYPE_BOOLEAN,
+            'dateCreated' => Schema::TYPE_INTEGER,
+            'dateUpdated' => Schema::TYPE_INTEGER,
+        ], $tableOptions);
+    }
+
+    public function safeDown()
+    {
+        $this->dropTable('{{%blogPost}}');
+    }
+}
