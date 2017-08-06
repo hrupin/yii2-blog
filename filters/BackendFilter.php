d266236591ad59c37886843f798debfcbc2721b8
hrupin <hrupin.work@yandex.ru>
2017-08-06 10:45:37 +0300

    Update BackendFilter.php

--- filters/BackendFilter.php
+++ filters/BackendFilter.php
@@ -1,1 +1,20 @@
+<?php
 
+namespace hrupin\blog\filters;
+
+use yii\web\NotFoundHttpException;
+use yii\base\ActionFilter;
+
+class BackendFilter extends ActionFilter
+{
+    
+    public $controllers = ['profile', 'recovery', 'registration', 'settings'];
+  
+    public function beforeAction($action)
+    {
+        if (in_array($action->controller->id, $this->controllers)) {
+            throw new NotFoundHttpException('Not found');
+        }
+        return true;
+    }
+}
